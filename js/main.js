12238e98cccee48d9ef832e25a7ce5dc27ec271f
oshinoshin <donateluro@gmail.com>
2021-06-10 11:32:34 +0900

    連続でプレイできるようにする処理の実装

--- js/main.js
+++ js/main.js
@@ -13,7 +13,7 @@
 
       this.stop = document.createElement('div');
       this.stop.textContent = 'STOP';
-      this.stop.classList.add('stop');
+      this.stop.classList.add('stop', 'inactive');
       this.stop.addEventListener('click', () => {
         if (this.stop.classList.contains('inactive')) {
           return;
@@ -26,6 +26,8 @@
 
         if (panelsLeft === 0) {
           checkResult();
+          spin.classList.remove('inactive');
+          panelsLeft = 3;
         }
       });
 
@@ -58,6 +60,11 @@
     unmatch() {
       this.img.classList.add('unmatched');
     }
+
+    activate() {
+      this.img.classList.remove('unmatched');
+      this.stop.classList.remove('inactive');
+    }
   }
 
   function checkResult() {
@@ -86,8 +93,8 @@
       return;
     }
     spin.classList.add('inactive');
-
     panels.forEach(panel => {
+      panel.activate();
       panel.spin();
     });
   });
