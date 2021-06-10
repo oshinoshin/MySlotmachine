020432429a49505ebe18f6e4a34b1036eb6846cd
oshinoshin <donateluro@gmail.com>
2021-06-10 10:44:56 +0900

    spinとstopボタンを押した時の処理

--- js/main.js
+++ js/main.js
@@ -7,17 +7,38 @@
       section.classList.add('panel');
 
       this.img = document.createElement('img');
-      this.img.src = 'img/seven.png';
+      this.img.src = this.getRandomImage();
+
+      this.timeoutId = undefined;
 
       this.stop = document.createElement('div');
       this.stop.textContent = 'STOP';
       this.stop.classList.add('stop');
+      this.stop.addEventListener('click', () => {
+        clearTimeout(this.timeoutId);
+      });
 
       section.appendChild(this.img);
       section.appendChild(this.stop);
 
       const main = document.querySelector('main');
       main.appendChild(section);
+    }
+
+    getRandomImage() {
+      const images = [
+        'img/seven.png',
+        'img/bell.png',
+        'img/cherry.png',
+      ];
+      return images[Math.floor(Math.random() * images.length)];
+    }
+
+    spin() {
+      this.img.src = this.getRandomImage();
+      this.timeoutId = setTimeout(() => {
+        this.spin();
+      }, 50);
     }
   }
 
@@ -26,4 +47,11 @@
     new Panel(),
     new Panel(),
   ];
+
+  const spin = document.getElementById('spin');
+  spin.addEventListener('click', () => {
+    panels.forEach(panel => {
+      panel.spin();
+    });
+  });
 }
